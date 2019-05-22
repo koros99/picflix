f8ed428981455ef42b577514082be412737c696c
koros99 <koronje254@gmail.com>
2019-05-22 14:26:54 +0300

    Add methods to handle parsing of data to layouts

--- app/src/main/java/com/kilel/picflix/RecyclerViewAdapter.java
+++ app/src/main/java/com/kilel/picflix/RecyclerViewAdapter.java
@@ -1,4 +1,62 @@
 package com.kilel.picflix;
 
-public class RecyclerViewAdapter {
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.kilel.picflix.model.UnsplashAPIResponse;
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.ImageViewHolder> {
+
+    private List<UnsplashAPIResponse> mPictures = new ArrayList<>();
+    private Context mContext;
+
+    public RecyclerViewAdapter(List<UnsplashAPIResponse> pictures, Context context) {
+        this.mPictures = pictures;
+        this.mContext = context;
+    }
+
+    @NonNull
+    @Override
+    public ImageViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int position) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.image_list, parent, false);
+        return new ImageViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ImageViewHolder holder, int position) {
+        holder.bindPicture(mPictures.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return mPictures.size();
+    }
+
+    public class ImageViewHolder extends RecyclerView.ViewHolder {
+        @BindView(R.id.img_id) ImageView mImageView;
+        @BindView(R.id.img_description_id) TextView mDescription;
+        private Context context;
+
+        public ImageViewHolder(View itemView) {
+            super(itemView);
+            ButterKnife.bind(this, itemView);
+            context = itemView.getContext();
+        }
+        public void bindPicture(UnsplashAPIResponse picture){
+            Picasso.get().load(picture.getUrls().getSmall()).into(mImageView);
+        }
+    }
 }
