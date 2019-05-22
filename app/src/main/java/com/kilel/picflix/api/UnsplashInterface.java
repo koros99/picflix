3a4593c5af9897ae7fd6748ed274c45dade03d04
koros99 <koronje254@gmail.com>
2019-05-22 14:19:53 +0300

    Add API interface that will contain the call request to get data

---
+++ app/src/main/java/com/kilel/picflix/api/UnsplashInterface.java
@@ -0,0 +1,15 @@
+package com.kilel.picflix.api;
+
+import com.kilel.proto_picflix.model.UnsplashAPIResponse;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+public interface UnsplashInterface {
+
+    @GET("/photos")
+    Call<List<UnsplashAPIResponse>> getPhotos(@Query("client_id") String apiKey);
+}
