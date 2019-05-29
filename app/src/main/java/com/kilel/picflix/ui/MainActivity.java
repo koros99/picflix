b42d6b5c8acc0fdf5f9faa15b3a78859660c2db8
koros99 <koronje254@gmail.com>
2019-05-29 15:12:44 +0300

    Add authentication to the application

--- app/src/main/java/com/kilel/picflix/ui/MainActivity.java
+++ app/src/main/java/com/kilel/picflix/ui/MainActivity.java
@@ -1,7 +1,10 @@
 package com.kilel.picflix.ui;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -11,8 +14,17 @@ import android.support.v7.widget.SearchView;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.IdpResponse;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.kilel.picflix.BuildConfig;
 import com.kilel.picflix.R;
 import com.kilel.picflix.adapters.RecyclerViewAdapter;
@@ -21,6 +33,7 @@ import com.kilel.picflix.api.UnsplashInterface;
 import com.kilel.picflix.model.UnsplashAPIResponse;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import butterknife.BindView;
@@ -40,17 +53,77 @@ public class MainActivity extends AppCompatActivity {
     private SharedPreferences.Editor mEditor;
     private String mRecentSearch;
 
+    List<AuthUI.IdpConfig> providers;
+    private static final int MY_REQUEST_CODE = 3002;
+
     RecyclerViewAdapter adapter;
 
     @BindView(R.id.recyclerView) RecyclerView mRecyclerView;
+    @BindView(R.id.btn_sign_out) Button btn_sign_out;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        btn_sign_out.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AuthUI.getInstance()
+                        .signOut(MainActivity.this)
+                        .addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                btn_sign_out.setEnabled(false);
+                                showSignInOptions();
+                            }
+                        }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(MainActivity.this, ""+e.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        });
+
+        providers = Arrays.asList(
+                new AuthUI.IdpConfig.EmailBuilder().build(),
+                new AuthUI.IdpConfig.PhoneBuilder().build(),
+                new AuthUI.IdpConfig.FacebookBuilder().build(),
+                new AuthUI.IdpConfig.GoogleBuilder().build()
+        );
+
+        showSignInOptions();
 
         ButterKnife.bind(this);
         LoadJson("");
+    }
+
+    private void showSignInOptions(){
+        startActivityForResult(
+                AuthUI.getInstance().createSignInIntentBuilder()
+                        .setAvailableProviders(providers)
+                        .setTheme(R.style.MyTheme)
+                        .build(), MY_REQUEST_CODE
+        );
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == MY_REQUEST_CODE){
+            IdpResponse response = IdpResponse.fromResultIntent(data);
+            if (resultCode == RESULT_OK){
+                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                Toast.makeText(this, "" + user.getEmail(), Toast.LENGTH_SHORT).show();
+                btn_sign_out.setEnabled(true);
+            }
+            else {
+                Toast.makeText(this, "" + response.getError().getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 
     @Override
@@ -94,7 +167,7 @@ public class MainActivity extends AppCompatActivity {
         UnsplashInterface apiInterface = ApiClient.getApiClient().create(UnsplashInterface.class);
         Call<List<UnsplashAPIResponse>> call;
 
-        if (keyword.length() > 0) {
+        if (keyword.length() != 0) {
             call = apiInterface.getSearchPhotos(API_KEY, LIMIT, keyword);
         }
         else {
